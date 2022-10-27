18ff825e244e18f881328c98ee35ebf2c8f12053
Layla <layla@layla.gg>
2022-10-27 02:34:13 +0000

    Allow full env config

--- app/configuration.go
+++ app/configuration.go
@@ -8,7 +8,7 @@ type DiscordConfig struct {
 	Token   string `yaml:"token" env:"DISCORD_TOKEN"`
 	GuildID string `yaml:"guild_id" env:"DISCORD_GUILD_ID"`
 
-	EventCategory       string `yaml:"event_category"`
-	ArchiveCategory     string `yaml:"archive_category"`
-	NotificationChannel string `yaml:"notification_channel"`
+	EventCategory       string `yaml:"event_category" env:"DISCORD_EVENT_CATEGORY"`
+	ArchiveCategory     string `yaml:"archive_category" env:"DISCORD_ARCHIVE_CATEGORY"`
+	NotificationChannel string `yaml:"notification_channel" env:"DISCORD_ARCHIVE_CATEGORY"`
 }
