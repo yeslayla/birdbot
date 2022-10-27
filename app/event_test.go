d8ca637c7d70701d6de9a8373fb65c4f234c3c05
Layla <layla@layla.gg>
2022-10-27 03:54:44 +0000

    Basic Testing & Cleanup

---
+++ app/event_test.go
@@ -0,0 +1,27 @@
+package app
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetChannelName(t *testing.T) {
+	assert := assert.New(t)
+
+	event := Event{
+		Name:     "Hello World",
+		Location: "1234 Place Rd, Ann Arbor, MI 00000",
+		DateTime: time.Date(2022, time.January, 5, 0, 0, 0, 0, time.UTC),
+	}
+	assert.Equal("jan-5-ann-arbor-hello-world", event.GetChannelName())
+
+	event = Event{
+		Name:     "Hello World",
+		Location: "Michigan Theater, Ann Arbor",
+		DateTime: time.Date(2022, time.January, 5, 0, 0, 0, 0, time.UTC),
+	}
+	assert.Equal("jan-5-hello-world", event.GetChannelName())
+
+}
